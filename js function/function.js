cb60b876ae566537bcd7f85f2c1afa19dc87386b
DemetrioPadre <demetriopadre@gmail.com>
2024-01-17 19:49:32 +0100

    fixed

--- js function/function.js
+++ js function/function.js
@@ -8,21 +8,34 @@
  * @returns numero randomico generato tra minimo e massimo
  */
 
+
+//     min = parseInt(min);
+//     max = parseInt(max);
+
+//     if (isNaN(min) || isNaN(max)) {
+//         console.error('I valori inseriti devono essere numerici');
+//         return;
+//     }
+
+//     if (min >= max) {
+//         console.error('Il valore massimo deve essere maggiore del valore minimo');
+//     }
 function getRandomNumber(min, max) {
-    min = parseInt(min);
-    max = parseInt(max);
-
-    if (isNaN(min) || isNaN(max)) {
-        console.error('I valori inseriti devono essere numerici');
-        return;
-    }
-
-    if (min >= max) {
-        console.error('Il valore massimo deve essere maggiore del valore minimo');
-    }
-
     const randomNumber = Math.floor(Math.random() * (max - min + 1) + min);
     return randomNumber;
 }
-const myRand = getRandomNumber(1, 6,);
-console.log(myRand);
+
+
+function getNumbers(number) {
+    if (number % 2 == 0) {
+        value = 'pari';
+        alert('il numero è pari');
+
+    } else {
+        value = 'dispari';
+        alert('il numero è dispari');
+    }
+
+    return number % 2 == 0;
+}
+
--- main js/main.js
+++ main js/main.js
@@ -1,10 +1,37 @@
 console.log('ciao padrone');
 
+let value;
+
 // dichiariamo le costanti e le stampiamo
 const scelta = prompt('utente mio fai la tua scelta che sia pari o che sia dispari');
-console.log(scelta);
+console.log('l utente ha scelto: ' + scelta);
+
+//scelta dell utente del numero
 const sceltaUtente = parseInt(prompt('Scegli un numeretto da 1 a 5'));
-console.log(sceltaUtente);
+console.log('il numero scelto dall utente è ' + sceltaUtente);
+
+
+//scelta del pc
+const sceltaPc = getRandomNumber(1, 5);
+console.log('li numero scelto del pc è ' + sceltaPc);
+
+
+let sum = sceltaUtente + sceltaPc;
+console.log('la somma è: ' + sum);
+
+
+console.log(getNumbers(sum));
+
+if (scelta == value) {
+    alert('hai vinto');
+} else {
+    alert('hai perso');
+};
+
+
+
+
+
 
 
 
